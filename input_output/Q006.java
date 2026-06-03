cc2c36712f2790d0d768c18b75232a4e99b12cd3
PAVITHRAN S <pavithrand2405@gmail.com>
2026-06-03 07:53:48 +0530

    Add files via upload

---
+++ input_output/Q006.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Q006{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        String msg=scan.nextLine();
+        System.out.println(msg);
+    }
+}
---
+++ input_output/Q007.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Q007{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        System.out.print("the given number is "+a);
+    }
+}
