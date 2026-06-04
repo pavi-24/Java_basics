8fb68b26994cf3c6bbb77f21177ed978bc9d7239
PAVITHRAN S <pavithrand2405@gmail.com>
2026-06-04 21:38:21 +0530

    Add files via upload

---
+++ input_output/Q008.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Q008{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        float a=scan.nextFloat();
+        System.out.println(a);
+    }
+}
---
+++ input_output/Q009.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Q009{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        float a=scan.nextFloat();
+        System.out.printf("%.2f",a);
+    }
+}
